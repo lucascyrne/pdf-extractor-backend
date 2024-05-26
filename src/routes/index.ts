f2fd1f5fa9a40bdb49e06442449448845042e956
lucascyrne <cyrne24@gmail.com>
2024-05-26 02:01:24 -0300

    fix: GCP deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -36,62 +36,75 @@ router.get('/invoices', async (req, res) => {
 });
 
 // Route to download an invoice
-router.get('/invoices/download/:fileName', cors(corsOptions), (req, res) => {
-  const fileName = req.params.fileName;
-  const baseDir = path.join(__dirname, '../faturas');
+router.get(
+  '/invoices/download/:fileName',
+  cors(corsOptions),
+  (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Adiciona header CORS diretamente
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, PUT, POST, DELETE, OPTIONS'
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  },
+  (req, res) => {
+    const fileName = req.params.fileName;
+    const baseDir = path.join(__dirname, '../faturas');
 
-  console.log(`Base directory: ${baseDir}`);
-  console.log(`File requested: ${fileName}`);
+    console.log(`Base directory: ${baseDir}`);
+    console.log(`File requested: ${fileName}`);
 
-  const findFileInSubdirectories = (
-    dir: string,
-    file: string,
-    depth: number = 0
-  ): string | null => {
-    if (depth > 10) {
-      console.error(`Reached max directory depth at ${dir}`);
-      return null;
-    }
+    const findFileInSubdirectories = (
+      dir: string,
+      file: string,
+      depth: number = 0
+    ): string | null => {
+      if (depth > 10) {
+        console.error(`Reached max directory depth at ${dir}`);
+        return null;
+      }
 
-    try {
-      console.log(`Searching in directory: ${dir}`);
-      const files = fs.readdirSync(dir);
-      for (const fileOrDir of files) {
-        const fullPath = path.join(dir, fileOrDir);
-        console.log(`Checking path: ${fullPath}`);
-        if (fs.lstatSync(fullPath).isDirectory()) {
-          const found = findFileInSubdirectories(fullPath, file, depth + 1);
-          if (found) return found;
-        } else if (fileOrDir === file) {
-          console.log(`File found: ${fullPath}`);
-          return fullPath;
+      try {
+        console.log(`Searching in directory: ${dir}`);
+        const files = fs.readdirSync(dir);
+        for (const fileOrDir of files) {
+          const fullPath = path.join(dir, fileOrDir);
+          console.log(`Checking path: ${fullPath}`);
+          if (fs.lstatSync(fullPath).isDirectory()) {
+            const found = findFileInSubdirectories(fullPath, file, depth + 1);
+            if (found) return found;
+          } else if (fileOrDir === file) {
+            console.log(`File found: ${fullPath}`);
+            return fullPath;
+          }
         }
+      } catch (error) {
+        console.error(`Error reading directory ${dir}:`, error);
+        return null;
       }
-    } catch (error) {
-      console.error(`Error reading directory ${dir}:`, error);
       return null;
+    };
+
+    if (!fs.existsSync(baseDir)) {
+      console.error(`Base directory ${baseDir} not found`);
+      return res.status(404).send('Base directory not found');
     }
-    return null;
-  };
 
-  if (!fs.existsSync(baseDir)) {
-    console.error(`Base directory ${baseDir} not found`);
-    return res.status(404).send('Base directory not found');
+    const filePath = findFileInSubdirectories(baseDir, fileName);
+    console.log(`File path found: ${filePath}`);
+
+    if (filePath) {
+      res.download(filePath, (err) => {
+        if (err) {
+          console.error(`Error during file download: ${err}`);
+        }
+      });
+    } else {
+      console.error(`File not found: ${fileName}`);
+      res.status(404).send('File not found');
+    }
   }
-
-  const filePath = findFileInSubdirectories(baseDir, fileName);
-  console.log(`File path found: ${filePath}`);
-
-  if (filePath) {
-    res.download(filePath, (err) => {
-      if (err) {
-        console.error(`Error during file download: ${err}`);
-      }
-    });
-  } else {
-    console.error(`File not found: ${fileName}`);
-    res.status(404).send('File not found');
-  }
-});
+);
 
 export default router;
